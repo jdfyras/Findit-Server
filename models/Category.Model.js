eaadf1954bd12e79844168acf92e3ff775a368c6
ykali <yakdhanali97@gmail.com>
2022-05-01 22:39:39 +0200

    setup

---
+++ models/Category.Model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+
+const CategorySchema = new mongoose.Schema({
+    name : {
+        type :String ,
+        required : false ,
+        unique :false
+    },
+    image : {
+        type : String,
+        required : true
+    }
+})
+
+module.exports = mongoose.model("Category", CategorySchema)
