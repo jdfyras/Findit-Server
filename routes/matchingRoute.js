49e543600eb5e406ed3bbb421097d1ccd9218931
JDAY FIRAS <jdfyras@gmail.com>
2022-06-15 20:43:11 +0100

    Update matchingRoute.js

--- routes/matchingRoute.js
+++ routes/matchingRoute.js
@@ -3,45 +3,60 @@ const objectModel = require('../models/objectModel')
 var fuzz = require('fuzzball')
 
 router.get('', async (req, res) => {
-    // console.log(params1)
-    // let query = params1
-    // let choices = params
     let found = await objectModel
         .findById('62a908b38ad39d8118c5f189')
         .populate('refCategory')
     console.log(found)
     let lost = await objectModel
-        .find({ statut: 'lost' })
+        .find({ statut: 'lost', refCategory: found.refCategory })
         .populate('refCategory')
     let query = found.formInput
-    query['categoryName'] = found.refCategory.name
+    query['id'] = found.id
+
     let choices = []
     for (let i in lost) {
-        let category = lost[i].refCategory
         let par = lost[i].formInput
-        par['categoryName'] = category.name
+        par['id'] = lost[i].id
         choices.push(par)
     }
     console.log(choices)
-    // function myCustomScorer(query, choice, options) {
-    //     if (query.categoryName !== choice.categoryName) return 0
-    //     else return fuzz.ratio(query.name, choice.name, options)
-    // }
+    
+    function myCustomScorer(query, choice, options) {
+        let processorQuery = ''
+        let processorChoice = ''
+        for (let elem in choice) {
+            console.log(elem)
+            if (elem !== 'id') {
+                processorQuery += query[`${elem}`]
+                processorChoice += choice[`${elem}`]
+            }
+        }
 
-    // let options = { scorer: myCustomScorer }
-    // let results = fuzz.extract(query, choices)
-    for (let choice of choices) {
-        choice.proc_sorted = fuzz.process_and_sort(
-            fuzz.full_process(choice.model)
-        )
+        return fuzz.ratio(processorQuery, processorChoice, {
+            returnObjects: true
+        })
     }
-    let options = {
-        scorer: fuzz.token_sort_ratio,
-        full_process: false
-    }
+
+    let options = { scorer: myCustomScorer, returnObjects: true }
     let results = fuzz.extract(query, choices, options)
     console.log(results)
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    const abortController = new AbortController()
 
+    options.abortController = abortController
+    options.asyncLoopOffset = 64
+
+    fuzz.extractAsPromised('gonna get aborted', choices, options)
+        .then((res) => {
+            /* do stuff */
+        })
+        .catch((e) => {
+            if (e.message === 'aborted') console.log('I got aborted!')
+        })
+
+    abortController.abort()
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
     res.json(results)
 })
 
